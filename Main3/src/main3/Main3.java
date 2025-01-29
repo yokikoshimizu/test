b5dd04b1c54cbb8e4d9c1d3502e509a3110ced83
yokikoshimizu <yokikoshimizu@gmail.com>
2025-01-29 21:45:35 +0900

    3-2 プッシュいたします

--- Main3/src/main3/Main3.java
+++ Main3/src/main3/Main3.java
@@ -1,3 +1,31 @@
 package main3;
-import java.io.*;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main3{
+	public static void main (String[] args) throws IOException {
+		BufferedReader br =
+				new BufferedReader (new InputStreamReader(System.in));
+		
+		System.out.println("整数 x を入力してください:");
+		String strx = br.readLine();
+		int x = Integer.parseInt(strx);
+		
+		System.out.println("整数 y を入力してください:");
+		String stry = br.readLine();
+		int y = Integer.parseInt(stry);
+		
+		if (x < y && x % 2 == 0 && y % 2 == 0) {
+			System.out.println("x は y より小さく、かつ、x と y は共に偶数である。");
+		}
+		if (x == y && x < 0) {
+			System.out.println("x と y は等しく、かつ、負の数である。");
+		}
+		if (x < y || x % 2 == 0) {
+			System.out.println("x は y より小さい、または、x は偶数である。");
+		}
+		
+	}
+}
