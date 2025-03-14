500889bc421719476966f2e4324fd984fb8f8975
heppo <heppo@192.168.10.50>
2025-03-14 16:30:14 +0900

    new commit

--- Main3/src/main3/Main3.java
+++ Main3/src/main3/Main3.java
@@ -4,10 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * メインクラス Main3。
+ */
+
 public class Main3 {
-	public static void main (String[] args) throws IOException
-	{
+	public static void main (String[] args) throws IOException {
 		System.out.println("0~100までの数字を入力してください");
+
+		/**
+	     * @param args コマンドライン引数の配列（未使用）
+	     * @throws IOException 入出力エラーが発生した場合にスローされる
+	     */
 		
 		BufferedReader br =
 				new BufferedReader (new InputStreamReader (System.in));
@@ -17,14 +25,11 @@ public class Main3 {
 		
 		if (score > 100 || score < 0) {
 			System.out.println("0~100の数字を入力してください");
-		}
-		else if (score >= 80) {
+		} else if (score >= 80) {
 			System.out.println("たいへんよくできました。");
-		}
-		else if (score < 80 || score >= 60) {
+		} else if (score < 80 || score >= 60) {
 			System.out.println("よくできました。");
-		}
-		else {
+		} else {
 			System.out.println("ざんねんでした。");
 		}
 	}
