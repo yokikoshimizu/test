6eb64cdb40f8678fbb65f78ee706d07df2687da8
heppo <heppo@192.168.1.248>
2025-03-29 16:20:53 +0900

    4-1 修正プッシュしました！

--- Main4/src/main4/Main4.java
+++ Main4/src/main4/Main4.java
@@ -4,7 +4,7 @@ public class Main4 {
      * プログラムのエントリーポイント。
      * 保険契約者リストを処理し、保険料および遅延利息金を表示する。
      *
-     * @param args コマンドライン引数（使用しない）
+     * @param args コマンドライン引数（使用しない）　
      */
 	public static void main (String[] args) 
 	{
